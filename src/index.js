8697fd26adfa9c21d6c0b35cf7cd576cc7ab9b67
Alina Voronaya <alinavoronaya@mail.ru>
2022-02-20 14:18:24 +0500

    Добавила JS скрипт с обработкой отправки формы

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ require('./styles/styles.scss');
 if (process.env.NODE_ENV === 'development') {
     require('../index.html');
 }
+
+const form = document.getElementById("form");
+
+form.onsubmit = function(event) {
+    event.preventDefault();
+    const form = event.target;
+    if (!form.elements["username"].value || !form.elements["telephone"].value || !form.elements["password"].value) {
+        alert('Необходимо заполнить форму');
+        return;
+    }
+    alert('Отлично! Отправляю...');
+};
